77706412001e9beccb4ad7699182813cbc194045
Jasmin Warner <55545634+warner97@users.noreply.github.com>
2021-11-24 18:56:32 -0500

    Add files via upload

---
+++ B1G_Buttons.php
@@ -0,0 +1,67 @@
+<?php
+error_reporting(E_ALL & ~E_NOTICE);
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+		<title>Revenue, Expenses, and Profit</title>
+
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.17/d3.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+	
+</head>
+
+	<header style="background-color:#FFFFFF; border:3px; border-color: #000000;  text-align:center;">
+		<img src="Images/B1G_logo.png" alt="B1G 10" width:"184px" height:"70px">
+	</header> 
+	
+<body>
+
+	<div id="container">
+		<div class="loggedIn">
+    </div>
+		
+	<table style="width: 100%; padding-right: 30px">
+  <tr>
+    <th style = "font-size: 25px; font-family: Impact, Haettenschweiler, 'Franklin Gothic Bold', 'Arial Black', 'sans-serif'">West Division</th>
+    <th style = "font-size: 25px; font-family: Impact, Haettenschweiler, 'Franklin Gothic Bold', 'Arial Black', 'sans-serif'">East Division</th>
+  </tr>
+  <tr>
+    <td style = "padding-left: 150px">
+		</br>
+		<img src="Images/illinois.png"     alt="Illinois"      width: "28px" height: "40px">&nbsp; &nbsp; &nbsp; &nbsp; 
+		<img src="Images/iowa.png"         alt="Iowa"          width: "71px" height: "40px">&nbsp; &nbsp; &nbsp; &nbsp;   
+		<img src="Images/minnesota.png"    alt="Minnesota"     width: "67px" height: "40px">&nbsp; &nbsp; &nbsp; &nbsp;  
+		<img src="Images/nebraska.png"     alt="Nebraska"      width: "40px" height: "40px">&nbsp; &nbsp; &nbsp; &nbsp; 
+		<img src="Images/northwestern.png" alt="Northwestern"  width: "26px" height: "40px">&nbsp; &nbsp; &nbsp; &nbsp;  
+		<img src="Images/purdue.png"       alt="Purdue"        width: "75px" height: "40px">&nbsp; &nbsp; &nbsp; &nbsp;  
+		<img src="Images/wisconsin.png"    alt="Wisconsin"     width: "42px" height: "40px">&nbsp; &nbsp; &nbsp; &nbsp;  
+	</td>
+        <td>
+		</br>
+		<img src="Images/indiana.png"    alt="Indiana"        width: "32px" height: "40px">&nbsp; &nbsp; &nbsp; &nbsp; 
+		<img src="Images/maryland.png"   alt="Maryland"       width: "37px" height: "40px">&nbsp; &nbsp; &nbsp; &nbsp;   
+		<img src="Images/umich.png"      alt="Michigan"       width: "55px" height: "40px">&nbsp; &nbsp; &nbsp; &nbsp;  
+		<img src="Images/msu.png"        alt="Michigan State" width: "35px" height: "40px">&nbsp; &nbsp; &nbsp; &nbsp; 
+		<img src="Images/osu.png"        alt="Ohio State"     width: "41px" height: "40px">&nbsp; &nbsp; &nbsp; &nbsp;  
+		<img src="Images/penn_state.png" alt="Penn State"     width: "58px" height: "40px">&nbsp; &nbsp; &nbsp; &nbsp;  
+		<img src="Images/rutgers.png"    alt="Rutgers"        width: "45px" height: "40px">&nbsp; &nbsp; &nbsp; &nbsp;  
+	</td>
+  </tr>
+  
+</table>
+   
+		</br> 
+
+
+
+</body>
+</html>
